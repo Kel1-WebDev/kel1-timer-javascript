c91ea9c0e9b1f98d9d752cbdc184c7085bb79d15
Hargi <hargimuhammad@gmail.com>
2021-09-22 14:15:03 +0700

    feature(stopwatch): delete milisecond

--- Stopwatch/script/stopwatch.js
+++ Stopwatch/script/stopwatch.js
@@ -10,20 +10,14 @@ var buttonStart = document.getElementById("start");
 var buttonStop = document.getElementById("stop");
 var buttonReset = document.getElementById("reset");
 var interval;
+const btn = document.getElementById("start");
 
 function startTimer(){
     tens ++;
-    if (tens<9){
-        appendTens.innerHTML = "0"+tens;
-    }
-    if (tens>9){
-        appendTens.innerHTML = tens;
-    }
     if (tens>99){
         second++;
         appendSecond.innerHTML = "0"+second;
         tens = 0;
-        appendTens.innerHTML = "0" + 0;
     }
     if (second > 9){
         appendSecond.innerHTML= second;
@@ -33,7 +27,6 @@ function startTimer(){
         appendMinute.innerHTML = "0"+minute;
         tens = 0;
         second = 0;
-        appendTens.innerHTML = "0" + 0;
         appendSecond.innerHTML = "0"+0;
     }
     if (minute > 9){
@@ -45,11 +38,11 @@ function startTimer(){
         tens = 0;
         second = 0;
         minute = 0;
-        appendTens.innerHTML = "0" + 0;
         appendSecond.innerHTML = "0" + 0;
         appendMinute.innerHTML = "0" + 0;
     }
 }
+
 buttonStart.onclick = function(){
     interval=setInterval(startTimer);
 };
@@ -60,12 +53,20 @@ buttonStop.onclick = function(){
 
 buttonReset.onclick = function(){
     clearInterval(interval);
-    tens = "00";
     second = "00";
     minute = "00";
     hour = "00";
     appendSecond.innerHTML = second;
-    appendTens.innerHTML = tens;
     appendMinute.innerHTML = minute;
     appendHour.innerHTML = hour;
 }
+
+
+btn.addEventListener("click",()=>{
+    if(btn.value=='start'){
+        btn.value ='pause'; 
+    }
+    else if (btn.value=='pause'){
+        btn.value ='resume';
+    }
+});
